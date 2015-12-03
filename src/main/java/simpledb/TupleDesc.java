85ebcc02517d2e9027183f7dfa085c8adfda0a25
lihao <lihaowhu@gmail.com>
2015-12-03 16:30:20 +0800

    Intial commit

---
+++ src/main/java/simpledb/TupleDesc.java
@@ -0,0 +1,231 @@
+package simpledb;
+
+import java.io.Serializable;
+import java.util.*;
+
+import simpledb.TupleDesc.TDItem;
+
+/**
+ * TupleDesc describes the schema of a tuple.
+ */
+public class TupleDesc implements Serializable {
+
+    /**
+     * A help class to facilitate organizing the information of each field
+     * */
+    public static class TDItem implements Serializable {
+
+        private static final long serialVersionUID = 1L;
+
+        /**
+         * The type of the field
+         * */
+        Type fieldType;
+
+        /**
+         * The name of the field
+         * */
+        String fieldName;
+
+        public TDItem(Type t, String n) {
+            this.fieldName = n;
+            this.fieldType = t;
+        }
+
+        public String toString() {
+            return fieldName + "(" + fieldType + ")";
+        }
+    }
+
+    private List<TDItem> tuple = new ArrayList<TDItem>();
+
+    /**
+     * @return
+     *        An iterator which iterates over all the field TDItems
+     *        that are included in this TupleDesc
+     * */
+    public Iterator<TDItem> iterator() {
+        // some code goes here
+        return tuple.iterator();
+
+    }
+
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Create a new TupleDesc with typeAr.length fields with fields of the
+     * specified types, with associated named fields.
+     *
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     * @param fieldAr
+     *            array specifying the names of the fields. Note that names may
+     *            be null.
+     */
+    public TupleDesc(Type[] typeAr, String[] fieldAr) {
+        for(int i =0; i < typeAr.length; i++){
+            tuple.add(new TDItem(typeAr[i],fieldAr[i]));
+        }
+
+        // some code goes here
+    }
+
+    /**
+     * Constructor. Create a new tuple desc with typeAr.length fields with
+     * fields of the specified types, with anonymous (unnamed) fields.
+     *
+     * @param typeAr
+     *            array specifying the number of and types of fields in this
+     *            TupleDesc. It must contain at least one entry.
+     */
+    public TupleDesc(Type[] typeAr) {
+        this(typeAr, new String[typeAr.length]);
+        // some code goes here
+    }
+
+    private TupleDesc(List<TDItem> tuple){
+        this.tuple = tuple;
+    }
+    /**
+     * @return the number of fields in this TupleDesc
+     */
+    public int numFields() {
+        // some code goes here
+        return tuple.size();
+    }
+
+    /**
+     * Gets the (possibly null) field name of the ith field of this TupleDesc.
+     *
+     * @param i
+     *            index of the field name to return. It must be a valid index.
+     * @return the name of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public String getFieldName(int i) throws NoSuchElementException {
+        // some code goes here
+        return tuple.get(i).fieldName;
+    }
+
+    /**
+     * Gets the type of the ith field of this TupleDesc.
+     *
+     * @param i
+     *            The index of the field to get the type of. It must be a valid
+     *            index.
+     * @return the type of the ith field
+     * @throws NoSuchElementException
+     *             if i is not a valid field reference.
+     */
+    public Type getFieldType(int i) throws NoSuchElementException {
+        // some code goes here
+        return tuple.get(i).fieldType;
+    }
+
+    /**
+     * Find the index of the field with a given name.
+     *
+     * @param name
+     *            name of the field.
+     * @return the index of the field that is first to have the given name.
+     * @throws NoSuchElementException
+     *             if no field with a matching name is found.
+     */
+    public int fieldNameToIndex(String name) throws NoSuchElementException {
+        // some code goes here
+        for(int i =0; i< tuple.size();i++){
+            if(tuple.get(i).fieldName!=null && tuple.get(i).fieldName.equals(name))
+                return i;
+        }
+
+        throw new NoSuchElementException();
+    }
+
+    /**
+     * @return The size (in bytes) of tuples corresponding to this TupleDesc.
+     *         Note that tuples from a given TupleDesc are of a fixed size.
+     */
+    public int getSize() {
+        // some code goes here
+        int i = 0;
+        for(TDItem item : tuple){
+            i += item.fieldType.getLen();
+        }
+        return i;
+    }
+
+    /**
+     * Merge two TupleDescs into one, with td1.numFields + td2.numFields fields,
+     * with the first td1.numFields coming from td1 and the remaining from td2.
+     *
+     * @param td1
+     *            The TupleDesc with the first fields of the new TupleDesc
+     * @param td2
+     *            The TupleDesc with the last fields of the TupleDesc
+     * @return the new TupleDesc
+     */
+    public static TupleDesc merge(TupleDesc td1, TupleDesc td2) {
+        // some code goes here
+        List<TDItem> tuple = new ArrayList<TDItem>();
+        tuple.addAll(td1.tuple);
+        tuple.addAll(td2.tuple);
+        return  new TupleDesc(tuple);
+
+    }
+
+    /**
+     * Compares the specified object with this TupleDesc for equality. Two
+     * TupleDescs are considered equal if they are the same size and if the n-th
+     * type in this TupleDesc is equal to the n-th type in td.
+     *
+     * @param o
+     *            the Object to be compared for equality with this TupleDesc.
+     * @return true if the object is equal to this TupleDesc.
+     */
+    public boolean equals(Object o) {
+        // some code goes here
+        if (o ==null || !o.getClass().getName().equals(TupleDesc.class.getName()))
+            return false;
+
+        TupleDesc td = (TupleDesc) o;
+        if(tuple.size() != td.tuple.size() )
+            return false;
+        for(int i =0;i<tuple.size();i++){
+            if(tuple.get(i).fieldType != td.tuple.get(i).fieldType)
+                return false;
+        }
+        return true;
+    }
+
+    public int hashCode() {
+        // If you want to use TupleDesc as keys for HashMap, implement this so
+        // that equal objects have equals hashCode() results
+        throw new UnsupportedOperationException("unimplemented");
+    }
+
+    /**
+     * Returns a String describing this descriptor. It should be of the form
+     * "fieldType[0](fieldName[0]), ..., fieldType[M](fieldName[M])", although
+     * the exact format does not matter.
+     *
+     * @return String describing this descriptor.
+     */
+    public String toString() {
+        // some code goes here
+        String str = new String();
+        for (int i=0;i<tuple.size();i++){
+            TDItem td = tuple.get(i);
+            str += td.fieldType.toString() + "[" + i+ "]("+td.fieldName+"["+i+"])";
+            if(i!=tuple.size()-1){
+                str+=",";
+            }
+        }
+
+
+
+        return str;
+    }
+}
